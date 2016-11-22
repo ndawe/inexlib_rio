ed32c8294564265a933936818f82a8f896ca50c9
Noel Dawe <noel.dawe@gmail.com>
2016-11-22 09:09:04 +0100

    merge iro.h and ibo.h into iro_ibo.h

--- inlib/root/iro_ibo.h
+++ inlib/root/iro_ibo.h
@@ -1,13 +1,13 @@
 // Copyright (C) 2010, Guy Barrand. All rights reserved.
 // See the file inlib.license for terms.
 
-#ifndef inlib_rroot_iro
-#define inlib_rroot_iro
+#ifndef inlib_root_iro_ibo
+#define inlib_root_iro_ibo
 
 #include <string>
 
 namespace inlib {
-    namespace rroot {
+    namespace root {
         class buffer;
     }
 }
@@ -15,14 +15,15 @@ namespace inlib {
 #include "../cid.h"
 
 namespace inlib {
-    namespace rroot {
+    namespace root {
 
-        class iro {
+        class iro_ibo {
         public:
-            virtual ~iro() {}
+            virtual ~iro_ibo() {}
         public:
             virtual void* cast(const std::string&) const = 0; //for ObjArray
-            virtual bool stream(buffer&) = 0;
+            virtual const std::string& store_cls() const = 0;
+            virtual bool stream(buffer&) const = 0;
 
             virtual const std::string& s_cls() const = 0;
             virtual iro* copy() const = 0;
@@ -30,6 +31,14 @@ namespace inlib {
             virtual void* cast(cid) const = 0; //OPTIMIZATION (geo).
         };
 
+    }
+
+    namespace wroot {
+        using namespace root;
+    }
+
+    namespace rroot {
+        using namespace root;
     }
 }
 
